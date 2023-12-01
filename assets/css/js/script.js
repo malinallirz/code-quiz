1b9b736e48b1700f6087cc9023d73fb695ca9580
malinallirz <105832267+malinallirz@users.noreply.github.com>
2023-11-30 22:15:56 -0500

    added answers and ability to click on them

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -18,6 +18,7 @@ function startTimer() {
     if (timeLeft === 0) {
       //stop timer
       clearInterval(interval);
+      document.getElementById("quiz-container").style.display = "none";
     }
   }, 1000);
   return interval;
@@ -26,29 +27,67 @@ function startTimer() {
 //array of questions for quiz
 var questions = [
   {
-    question: "Question 1",
-    options: ["Option 1", "Option 2", "Option 3"],
+    question: "What is 2*10?",
+    options: ["20", "12", "200"],
   },
   {
-    question: "Question 2",
-    options: ["Option 1", "Option 2", "Option 3"],
+    question: "what is 3 cubed?",
+    options: ["27", "6", "9"],
   },
   {
-    question: "Question 3",
-    options: ["Option 1", "Option 2", "Option 3"],
+    question: "how many sides does a triangle have?",
+    options: ["3", "6", "4"],
   },
   {
-    question: "Question 4",
-    options: ["Option 1", "Option 2", "Option 3"],
+    question: "complete the pattern: 25,26,28,31,35,_ ",
+    options: ["40", "36", "38"],
   },
 ];
 
+var currentQuestionIndex = 0;
+
+//display questions and answer options
+function displayQuestion() {
+  var question = questions[currentQuestionIndex];
+  document.getElementById("question").textContent = question.question;
+
+  var optionsList = document.getElementById("options");
+  optionsList.innerHTML = "";
+
+  question.options.forEach(function (option) {
+    var li = document.createElement("li");
+    li.textContent = option;
+    li.addEventListener("click", handleOptionSelection);
+    optionsList.appendChild(li);
+  });
+}
+
 function startQuiz() {
-  console.log("button works.game will start");
+  //console.log("button works.game will start");
   // pressing this button will take user to the quiz questions
   //timer starts
   var timerInterval = startTimer();
+  document.getElementById("start-page").style.display = "none";
+  document.getElementById("quiz-container").style.display = "block";
+  displayQuestion();
   // questions pop up
+}
+function handleOptionSelection() {
+  var selectedOption = this.textContent;
+
+  var question = questions[currentQuestionIndex];
+  var answer = question.options[0];
+  if (selectedOption === answer) {
+    console.log("correct");
+  } else {
+    console.log("wrong");
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    console.log("complete");
+  } else {
+    displayQuestion();
+  }
 }
 
 //user interactions
